2e109f3eb638834c7f0a4b3ed976459429ebcde5
Joar Johnson <Joar.DTK.Johnson@lystkom.se>
2018-11-22 14:10:29 +0100

    cool

--- projects/elev-javascript 101/assets/js/5-array.js
+++ projects/elev-javascript 101/assets/js/5-array.js
@@ -5,3 +5,38 @@ var friend3 = "stefan"
 var friend4 = "christopher"
 
 // list group of friends
+
+var cuntries = ["swedistan", "Noway", "WINLAND", "Denmark", "Island"]
+// cuntries.foreach(function(country) {
+//     console.log(country)
+// })
+
+// function cuntryList(a) {
+//     console.log("************")
+//     console.log(country)
+//     console.log("************")
+//     console.log("")
+
+// }
+
+function printReverse(arr) {
+    var newArray = [];
+    for (var i = arr.length - 1; i >= 0; i--) {
+      newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+   console.log(printReverse(cuntries))
+
+   
+    function isIdentical(arr1) {
+        for(var i = 1; i < arr1.length; i++)
+        {
+            if(arr1[i] !== arr1[0])
+                return false;
+        }
+    
+        return true;
+    }
+
+   console.log(isIdentical([1,1,1]))
