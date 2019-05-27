d8ada6e32fc4fba1d8e917d95c7dd6835a0e7841
johnsonjoar7@gmail.com <johnsonjoar7@gmail.com>
2019-05-27 16:24:19 +0200

    klar

--- projects/JsPlayground/assets/js/JsPlayGround.js
+++ projects/JsPlayground/assets/js/JsPlayGround.js
@@ -1,10 +1,11 @@
+// box 1 Lightbox
 var LoginBtn = document.getElementById("login")
 var lightBox = document.getElementById("LB")
 var wrapper = document.getElementById("found")
 
 LoginBtn.addEventListener("click", function() {
     lightBox.style.visibility = "visible";
-    document.body.style.backgroundColor = " rgb(29, 6, 6)";
+    document.body.style.backgroundColor = " rgb(29, 26, 26)";
 })
 
 var exitLightBox = document.getElementById("EX")
@@ -13,7 +14,7 @@ exitLightBox.addEventListener("click", function(){
     document.body.style.backgroundColor = " rgb(150, 31, 31)";
 })
   
-
+//box 2 Scorekeeper
 var p1Score = 0
 var p2Score = 0
 var winningScore = 0
@@ -78,4 +79,86 @@ resetBtn.addEventListener("click", function() {
     p2Display.style.color = "black"
     p1Display.style.color = "black"
     })
+
+    //box 4 Timer
+    var h1 = document.getElementById('timer'),
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+    function add() {
+        seconds++;
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+            }
+        }
+    
+            h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+        timer();
+    }
+    function timer() {
+        t = setTimeout(add, 1000);
+    }
+
+    start.onclick = timer;
+    stop.onclick = function() {
+        clearTimeout(t);
+    }
+    clear.onclick = function() {
+        h1.textContent = "00:00:00";
+        seconds = 0; minutes = 0; hours = 0;
+    }
+
+
+//box 5 darkmode
+
+var dmSwitch = document.getElementsByClassName("btndm")[0];
+var nightmode = false;
+var dmText = document.getElementById("dmText");
+
+dmSwitch.addEventListener("click", function(){
+    if(nightmode===false){
+        document.body.style.backgroundColor = "rgb(29, 27, 23)";
+        nightmode = true;
+        dmText.innerHTML = "Darkmode Enabled";
+    }
+    else{
+        nightmode = false;
+        document.body.style.backgroundColor = "rgb(150, 31, 31)";
+        dmText.innerHTML = "";
+
+    }
+});
+
+//box 3 smiley
+var img = document.getElementById("smile");
+var test = 0;
+var punch = document.getElementById("Punch").addEventListener("click", function(){
+    img.src = "assets/img/sadSmile.png";
+})
+
+var forgive = document.getElementById("Apolo").addEventListener("click", function(){
+    img.src = "assets/img/Happy.jpg";
+})
+
+//
+
+var inputText = document.getElementById("bloggPost");
+var input = document.getElementById("blogg");
+input.addEventListener("input", function() {
+    
+    inputText.innerHTML = this.value;
+    test = 1;
+
+})
+
+
+
 
